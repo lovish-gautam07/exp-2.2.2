cfea4c62fe1ff3fef7f95d410f1a30ff1b7a86e0
Lovish Gautam <lovishgautam.997@gmail.com>
2026-03-24 13:53:03 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Banking JWT API
+
+## Run project
+npm install
+npm run dev
+
+## Create .env file
+Copy .env.example to .env and add MongoDB URI.
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    balance: {
+        type: Number,
+        default: 1000
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
